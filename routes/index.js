17065365cef66b384e946b794ab60956c5b4a119
Brian Nieves <ban20@duke.edu>
2018-11-09 01:55:21 +0000

    Migration to express complete

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.sendFile("/var/www/dti/views/index.html");
+});
+
+module.exports = router;
