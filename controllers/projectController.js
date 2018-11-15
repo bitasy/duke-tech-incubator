9850209fcbe3b2d314efbd4e3b4d83f01e5b204b
Brian Nieves <ban20@duke.edu>
2018-11-15 21:09:51 +0000

    Added handlebars dynamic project loading to main index page

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -4,7 +4,12 @@ exports.projects_by = function (req, res) {
     //res.send(req.params.netid);
 
     Project.find("founder", req.params.netid, function (results) {
-        res.render("query", {founder: req.params.netid, results: results});
+        res.render("query", {title: req.params.netid + " Projects", founder: req.params.netid, results: results});
     })
+};
 
+exports.all = function (req, res) {
+    Project.all(function (projects) {
+        res.render("index", {title: "DTI", projects: projects})
+    })
 };
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,22 +1,4 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'dti',
-  password : 'incubator',
-  database : 'dti'
-});
-
-connection.connect();
-
-connection.query('SELECT name, description from Project', function (error, results, fields) {
-  if (error) throw error;
-  var names = [];
-  var descriptions = [];
-  for (var i in results){
-      names.push(results[i].name);
-      descriptions.push(results[i].description);
-  }
-  console.log(names, descriptions);
-});
-
-connection.end();
+//To search for projects
+//Get DOM form element
+//Send requets to http://dti.bitasy.me.node?query=DEFT -- NEEDS to be defined by some route handler first
+//returns array of string
