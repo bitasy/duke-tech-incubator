d394a8030cd06ec4b39573216676eebb468acb75
Jeffrey Li <jql3@duke.edu>
2018-12-08 21:57:21 +0000

    merge

---
+++ routes/forms.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/project', function (req, res) {
+    var name = req.body.username;
+    console.log(name);
+    res.render("formsubmit");
+});
+
+router.get('/', function (req, res){
+    res.send("in post");
+})
+
+module.exports = router;
